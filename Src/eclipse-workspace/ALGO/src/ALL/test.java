edafa9dc864678789f6e738f6c9d7eec9cd8b0e0
dlgmldnd017 <42001306+dlgmldnd017@users.noreply.github.com>
2022-10-19 22:45:09 +0900

    Algorithm_JAVA
    To correct the TRIANGLEPATH1.java, TRIANGLEPATH2.java

--- Src/eclipse-workspace/ALGO/src/ALL/test.java
+++ Src/eclipse-workspace/ALGO/src/ALL/test.java
@@ -9,6 +9,10 @@ public class test {
 	private static BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
 	private static StringBuilder sb = new StringBuilder();
 	
+	// 각각 삼각형의 크기 변수와 삼각형 안의 숫자 변수
+	private static int N;
+	private static int[][] triangle;
+	
 	private static int[][] cache;
 	
 	public static void main(String[] args) throws IOException {
@@ -17,13 +21,31 @@ public class test {
 		
 		for(int i=0; i<C; i++) {
 			cache = new int[101][101];
+			N = Integer.parseInt(sc.readLine());
+			triangle = new int[N][N];
 			
-			//sb.append(solve()+"\n");
+			for(int y=0; y<N; y++) {
+				st = new StringTokenizer(sc.readLine());
+				for(int x=0; x<N; x++) {
+					triangle[y][x] = Integer.parseInt(st.nextToken()); 
+				}
+			}
+			sb.append(solve(0,0)+"\n");
 		}
 		System.out.println(sb);
 	}
 	
-	private static void solve() {
+	private static int solve(int y, int x) {
+		if(triangle[y][x] == 0) return 0;
 		
+		// 기저 사례(1): 맨 밑층(N-1)까지 도달할 경우
+		if(y==N-1) return triangle[y][x];
+		
+		if(cache[y][x] != 0) return cache[y][x];
+		
+		int ret=0;
+		ret = triangle[y][x] + Math.max(solve(y+1, x), solve(y+1, x+1));
+		
+		return cache[y][x] = ret;
 	}
 }
--- Src/eclipse-workspace/ALGO/src/Dynamic_Programing_1/TRIANGLEPATH2.java
+++ Src/eclipse-workspace/ALGO/src/Dynamic_Programing_1/TRIANGLEPATH2.java
@@ -9,47 +9,43 @@ public class TRIANGLEPATH2 {
 	private static BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
 	private static StringBuilder sb = new StringBuilder();
 	
-	// 각각 캐시, 삼각형, 높이를 뜻한다.
-	private static int[][] cache;
-	private static int[][] triangle;
+	// 각각 삼각형의 크기 변수와 삼각형 안의 숫자 변수
 	private static int N;
-
+	private static int[][] triangle;
+	
+	private static int[][] cache;
+	
 	public static void main(String[] args) throws IOException {
 		StringTokenizer st;
 		int C = Integer.parseInt(sc.readLine());
 		
 		for(int i=0; i<C; i++) {
+			cache = new int[101][101];
 			N = Integer.parseInt(sc.readLine());
-			cache = new int[100][100];
-			
 			triangle = new int[N][N];
 			
 			for(int y=0; y<N; y++) {
 				st = new StringTokenizer(sc.readLine());
 				for(int x=0; x<N; x++) {
-					triangle[y][x] = Integer.parseInt(st.nextToken());
+					triangle[y][x] = Integer.parseInt(st.nextToken()); 
 				}
 			}
 			sb.append(solve(0,0)+"\n");
 		}
 		System.out.println(sb);
 	}
+	
 	private static int solve(int y, int x) {
-		// ex) 6 0 0 0 0에서 0은 빈 공간을 의미하기 때문에
-		// 바로 return 0;
 		if(triangle[y][x] == 0) return 0;
-				
-		// 맨 아래층에 도달 시, 최종 sum값 리턴
+		
+		// 기저 사례(1): 맨 밑층(N-1)까지 도달할 경우
 		if(y==N-1) return triangle[y][x];
 		
-		// 캐시 히트라면, 캐시 값 리턴
-		if(cache[y][x] != 0) {
-			System.out.println("cache hit! "+y+":"+x);
-			System.out.println(cache[y][x]);
-			return cache[y][x];
-		}
+		if(cache[y][x] != 0) return cache[y][x];
 		
-		// 부분 합 리턴하기
-		return cache[y][x] = Math.max(solve(y+1, x), solve(y+1, x+1)) + triangle[y][x];
+		int ret=0;
+		ret = triangle[y][x] + Math.max(solve(y+1, x), solve(y+1, x+1));
+		
+		return cache[y][x] = ret;
 	}
 }
